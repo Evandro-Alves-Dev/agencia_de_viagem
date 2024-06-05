2d2a7224f50bdb21129ddfdac4887433334f7c7a
Evandro Alves <evandroacer3@gmail.com>
2024-06-05 08:40:57 -0300

    first commit

---
+++ agencia_de_viagem/src/test/java/br/com/velg/agencia_de_viagem/AgenciaDeViagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.velg.agencia_de_viagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaDeViagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
